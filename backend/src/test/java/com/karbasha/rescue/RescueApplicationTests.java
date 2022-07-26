f675382610e7c38cfb56d4a524406cc6b50707ff
aalrahma <abdulrahman.alrahma@leantech.me>
2022-07-26 19:30:29 +0300

    first

---
+++ backend/src/test/java/com/karbasha/rescue/RescueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karbasha.rescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
